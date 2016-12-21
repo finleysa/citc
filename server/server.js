f3d80d1c39b184e46a51b6ac2c9cc6146066982a
Steven Finley <steve.a.finley@gmail.com>
2016-12-21 05:19:41 +0400

    edited css. changed hidepoweredby setting to php.

--- server/server.js
+++ server/server.js
@@ -3,10 +3,12 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const helmet = require('helmet')
+const winston = require('winston');
 
 var app = express()
 
 app.use(helmet());
+app.use(helmet.hidePoweredBy({ setTo: 'PHP 4.2.0' }))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -19,10 +21,13 @@ app.get('*', function(req, res) {
   res.status(200).sendFile(path.join(__dirname, '../dist/index.html'));
 })
 
+if (app.get('env') === 'production') {
+  console.log("Running server in production mode");
+}
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  logger.error("Route not Found")
   err.status = 404;
   next(err);
 });
@@ -31,6 +36,7 @@ app.use(function(req, res, next) {
 // will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
+    logger.error(err.message);
     res.status(err.status || 500);
     res.send(err.message);
   });
@@ -39,6 +45,7 @@ if (app.get('env') === 'development') {
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
+  logger.error(err.message);
   res.status(err.status || 500);
   res.render(err.message);
 });
@@ -46,7 +53,8 @@ app.use(function(err, req, res, next) {
 app.set('port', (process.env.PORT || 8080))
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+  console.log("Listening on localhost:" + app.get('port'))
 });
+
 
 // module.exports = app;
